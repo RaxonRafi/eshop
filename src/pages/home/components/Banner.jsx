89bea7e8ad909bd6989f571a087306bd6158e3a5
Rafi <itzmeh320@gmail.com>
2025-05-29 21:43:22 +0600

    Banner Added

---
+++ src/pages/home/components/Banner.jsx
@@ -0,0 +1,11 @@
+import bannerImage from '../../../assets/images/banner.jpg';
+
+const Banner = () => {
+  return (
+    <div className="mt-8">
+        <img src={bannerImage}/>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 import Container from '../../globalComponents/Container'
-import { useTranslation } from 'react-i18next'
 import { Helmet } from 'react-helmet-async'
+import Banner from './components/Banner'
+import SupportSection from './components/support'
 
 const Home = () => {
-  const {t} = useTranslation()
+ 
   return (
     <>
       <Helmet>
         <title>Home</title>
       </Helmet>
       <Container>
-        <h1>{t('this_is_home_page')}</h1>
+        <Banner/>
+        <SupportSection/>
       </Container>
     </>
   )
